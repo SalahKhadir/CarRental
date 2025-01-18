000df29eb665e61fb710126ed12a9e5c72a46fd4
salah <salaho.khadir@gmail.com>
2025-01-18 19:11:30 +0100

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   resources/views/register.blade.php

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -11,7 +11,7 @@
 <body>
     <nav class="navbar navbar-expand-lg navbar-light bg-light shadow-sm">
         <div class="container">
-            <a class="navbar-brand text-info fw-bold fs-4" href="#">CarRental</a>
+            <a class="navbar-brand text-info fw-bold fs-4" href="/">CarRental</a>
         </div>
     </nav>
 
